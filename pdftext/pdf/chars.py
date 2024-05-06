ebace908c3f4ca97a93bb5133267aa766fb8f875
Vik Paruchuri <vik.paruchuri@gmail.com>
2024-05-06 10:25:22 -0700

    Work around pdfium bug

--- pdftext/pdf/chars.py
+++ pdftext/pdf/chars.py
@@ -75,7 +75,7 @@ def get_pdfium_chars(pdf, fontname_sample_freq=settings.FONTNAME_SAMPLE_FREQ, pa
 
             rotation = pdfium_c.FPDFText_GetCharAngle(text_page, i)
             rotation = rotation * 180 / math.pi # convert from radians to degrees
-            coords = text_page.get_charbox(i, loose=True)
+            coords = text_page.get_charbox(i, loose=rotation == 0) # Loose doesn't work properly when page is rotated
             device_coords = page_bbox_to_device_bbox(page, coords, page_width, page_height, bl_origin, page_rotation, normalize=True)
 
             char_info = {
