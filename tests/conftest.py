0b3b4eeaa6f9abece820be369db78d2c2634d62b
stancld <daniel.stancl@gmail.com>
2025-10-07 13:33:03 +0200

    Convert correctly PDFium UTF-8 integers to char/string

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,10 @@ def pdf_path():
 def pdf_path2():
     return "tests/data/communication.pdf"
 
+@pytest.fixture(scope="session")
+def pdf_with_non_unicode_chars():
+    return "tests/data/non_unicode_chars.pdf"
+
 @pytest.fixture()
 def pdf_doc(pdf_path):
     doc = pdfium.PdfDocument(pdf_path)
